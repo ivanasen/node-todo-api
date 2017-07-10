ace685cc8734d7d561ec0317606edbb89e543d03
vankata9907 <vankata9907@gmail.com>
2017-07-10 19:03:24 +0300

    added tests for PATCH /todos/:id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -5,16 +5,15 @@ const request = require('supertest');
 const {app} = require('../server');
 const {Todo} = require('../db/models/todo');
 
-const testTodos = [
-    {
-        _id: new ObjectID(),
-        text: 'First test todo'
-    },
-    {
-        _id: new ObjectID(),
-        text: 'Second test todo'
-    }
-];
+const testTodos = [{
+    _id: new ObjectID(),
+    text: 'First test todo'
+}, {
+    _id: new ObjectID(),
+    text: 'Second test todo',
+    completed: true,
+    completedAt: 333
+}];
 
 beforeEach((done) => {
     Todo.remove({}).then(() => Todo.insertMany(testTodos))
@@ -104,5 +103,84 @@ describe('GET /todos/:id', () => {
             .get(`/todos/asdsfgffhtrh`)
             .expect(404)
             .end(done);
+    });
+});
+
+describe('DELETE /todos/:id', () => {
+    it('should delete a todo', (done) => {
+        request(app)
+            .delete(`/todos/${testTodos[0]._id.toHexString()}`)
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todo.text).to.equal(testTodos[0].text);                
+            })
+            .end((err, res) => {
+                if (err) {
+                    return done(err);
+                }
+
+                Todo.find(res).then((todo) => {
+                    expect(todo).to.not.exist();
+                }).catch(e => done(e));
+                done();
+            });
+    });
+
+    it('should return 404 if todo not found', (done) => {
+        request(app)
+            .delete(`/todos/${new ObjectID()}`)
+            .expect(404)
+            .end(done);
+    });
+
+    it('should return 400 non object valid ids', (done) => {
+        request(app)
+            .delete(`/todos/asdsfgffhtrh`)
+            .expect(404)
+            .end(done);
+    });
+});
+
+describe('PATCH /todos/:id', () => {
+    it('should update the todo', (done) => {
+        const update = {
+                text: 'Hellou Peshou',
+                completed: true
+        };
+
+        request(app)
+            .patch(`/todos/${testTodos[0]._id}`)
+            .send(update)
+            .expect(200)
+            .end((err, res) => {
+                if (err) {
+                    return done(err);
+                }
+
+                const {todo} = res.body;
+                expect(todo.text).to.equal(update.text);
+                expect(todo.completed).to.equal(update.completed);
+                expect(todo.completedAt).to.be.a('number');
+                done();
+            });    
+    });
+
+    it('should clear completedAt when todo is not completed', (done) => {
+        request(app)
+            .patch(`/todos/${testTodos[1]._id}`)
+            .send({
+                completed: false
+            })
+            .expect(200)
+            .end((err, res) => {
+                if (err) {
+                    return done(err);
+                }
+
+                const {todo} = res.body;
+                expect(todo.completed).to.equal(false);
+                expect(todo.completedAt).to.not.exist;
+                done();
+            });
     });
 });
